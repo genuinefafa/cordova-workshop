f26c64c93741d025b3cf3bef51f2b621616663ca
Facundo D <genuinefafa@gmail.com>
2014-07-11 12:27:50 -0300

    we now have handlebar included with some nice css stylying

--- www/js/app.js
+++ www/js/app.js
@@ -1,7 +1,13 @@
 // We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
 (function () {
 
+    /* -------------------------------- Templates ------------------------------------------ */
+    var homeTpl = Handlebars.compile($("#home-tpl").html());
+    var employeeListTpl = Handlebars.compile($("#employee-list-tpl").html());
+
     /* ---------------------------------- Local Variables ---------------------------------- */
+
+
     var service = new EmployeeService();
     service.initialize().done(function () {
         console.log("Service initialized");
@@ -27,22 +33,12 @@
     /* ---------------------------------- Local Functions ---------------------------------- */
     function findByName() {
         service.findByName($('.search-key').val()).done(function (employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i = 0; i < l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+          $('.content').html(employeeListTpl(employees));
         });
     }
 
     function renderHomeView() {
-      var html =
-        "<h1>Directory</h1>" +
-        "<input class='search-key' type='search' placeholder='Enter name'/>" +
-        "<ul class='employee-list'></ul>";
-      $('body').html(html);
+      $('body').html(homeTpl());
       $('.search-key').on('keyup', findByName);
     }
 
